3ec9db98538fa288f71932b2ab08f2aa96659c7f
Lord-Valen <lord_valen@protonmail.com>
2022-07-17 15:45:23 -0400

    Disable IPv6 by default

--- modules/networking/default.nix
+++ modules/networking/default.nix
@@ -4,13 +4,22 @@ with lib;
 
 let cfg = config.modules.networking;
 in {
-  options.modules.networking = { enable = mkEnableOption "Networking module"; };
+  options.modules.networking = {
+    enable = mkEnableOption "Networking module";
+    IPv6 = mkOption {
+      type = types.bool;
+      default = false;
+    };
+  };
 
   config = mkIf cfg.enable {
-    networking.wireless.iwd.enable = true;
-    networking.networkmanager = {
-      enable = true;
-      wifi.backend = "iwd";
+    networking = {
+      enableIPv6 = mkIf cfg.IPv6 true;
+      wireless.iwd.enable = true;
+      networkmanager = {
+        enable = true;
+        wifi.backend = "iwd";
+      };
     };
     programs.nm-applet.enable = true;
   };
