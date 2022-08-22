86376646d0de20b51d35bf40ba317091f0f29a98
Lord-Valen <lord_valen@protonmail.com>
2022-08-22 04:10:45 +0000

    :bug: `fix` Enable podman

--- profiles/arion/common.nix
+++ profiles/arion/common.nix
@@ -3,5 +3,13 @@
 {
   environment.systemPackages = with pkgs; [ arion docker-client ];
 
-  virtualisation.arion = { backend = "podman-socket"; };
+  virtualisation = {
+    docker.enable = false;
+    podman = {
+      enable = true;
+      dockerSocket.enable = true;
+      defaultNetwork.dnsname.enable = true;
+    };
+    arion = { backend = "podman-socket"; };
+  };
 }
