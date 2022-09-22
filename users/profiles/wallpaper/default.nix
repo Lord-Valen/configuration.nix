93244973761041a92afb8a491bf38db2afaa67b0
Lord-Valen <lord_valen@protonmail.com>
2022-09-22 15:43:41 -0400

    :rocket: `feat` Add random-background

--- users/profiles/wallpaper/default.nix
+++ users/profiles/wallpaper/default.nix
@@ -1,5 +1,12 @@
 { config, lib, pkgs, ... }:
 
 {
-  home.file.".background-image".source = ./wallpaper.d;
+  services.random-background = {
+    enable = true;
+    enableXinerama = true;
+    imageDirectory = "%h/.config/wallpaper";
+    interval = "1h";
+  };
+
+  xdg.configFile."wallpaper".source = ./wallpaper.d;
 }
