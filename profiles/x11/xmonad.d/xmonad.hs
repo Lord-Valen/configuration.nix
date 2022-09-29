108706b7d19c2eae2f8f707482b71492a1b26d59
Lord-Valen <lord_valen@protonmail.com>
2022-09-29 12:38:29 -0400

    :bug: `fix` Fix term scratchpad

--- profiles/x11/xmonad.d/xmonad.hs
+++ profiles/x11/xmonad.d/xmonad.hs
@@ -1,31 +1,76 @@
-import Data.Char (isSpace, toUpper)
+import Data.Char
+  ( isSpace,
+    toUpper,
+  )
 import qualified Data.Map as M
-import Data.Maybe (fromJust, isJust)
+import Data.Maybe
+  ( fromJust,
+    isJust,
+  )
 import Data.Monoid
 import System.Exit (exitSuccess)
 import System.IO (hPutStrLn)
 import XMonad
 import XMonad.Actions.CopyWindow (kill1)
-import XMonad.Actions.CycleWS (Direction1D (..), WSType (..), moveTo, nextScreen, prevScreen, shiftTo)
+import XMonad.Actions.CycleWS
+  ( Direction1D (..),
+    WSType (..),
+    moveTo,
+    nextScreen,
+    prevScreen,
+    shiftTo,
+  )
 import XMonad.Actions.MouseResize
 import XMonad.Actions.Promote
-import XMonad.Actions.RotSlaves (rotAllDown, rotSlavesDown)
+import XMonad.Actions.RotSlaves
+  ( rotAllDown,
+    rotSlavesDown,
+  )
 import qualified XMonad.Actions.Search as S
 import XMonad.Actions.Submap
 import XMonad.Actions.WindowGo (runOrRaise)
-import XMonad.Actions.WithAll (killAll, sinkAll)
+import XMonad.Actions.WithAll
+  ( killAll,
+    sinkAll,
+  )
 import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ManageDocks (ToggleStruts (..), avoidStruts, docks, manageDocks)
-import XMonad.Hooks.ManageHelpers (doCenterFloat, doFullFloat, isFullscreen)
+import XMonad.Hooks.ManageDocks
+  ( ToggleStruts (..),
+    avoidStruts,
+    docks,
+    manageDocks,
+  )
+import XMonad.Hooks.ManageHelpers
+  ( doCenterFloat,
+    doFullFloat,
+    isFullscreen,
+  )
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 import XMonad.Layout.GridVariants (Grid (Grid))
 import XMonad.Layout.LayoutModifier
-import XMonad.Layout.LimitWindows (decreaseLimit, increaseLimit, limitWindows)
-import XMonad.Layout.MultiToggle (EOT (EOT), mkToggle, single, (??))
-import qualified XMonad.Layout.MultiToggle as MT (Toggle (..))
-import XMonad.Layout.MultiToggle.Instances (StdTransformers (MIRROR, NBFULL, NOBORDERS))
+import XMonad.Layout.LimitWindows
+  ( decreaseLimit,
+    increaseLimit,
+    limitWindows,
+  )
+import XMonad.Layout.MultiToggle
+  ( EOT (EOT),
+    mkToggle,
+    single,
+    (??),
+  )
+import qualified XMonad.Layout.MultiToggle as MT
+  ( Toggle (..),
+  )
+import XMonad.Layout.MultiToggle.Instances
+  ( StdTransformers
+      ( NBFULL,
+        NOBORDERS
+      ),
+  )
 import XMonad.Layout.NoBorders
+import XMonad.Layout.Reflect (reflectHoriz)
 import XMonad.Layout.Renamed
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.ShowWName
@@ -34,53 +79,65 @@ import XMonad.Layout.Spacing
 import XMonad.Layout.SubLayouts
 import XMonad.Layout.Tabbed
 import XMonad.Layout.ThreeColumns
-import XMonad.Layout.WindowArranger (WindowArrangerMsg (..), windowArrange)
+import XMonad.Layout.WindowArranger
+  ( WindowArrangerMsg (..),
+    windowArrange,
+  )
 import XMonad.Layout.WindowNavigation
 import qualified XMonad.StackSet as W
-import XMonad.Util.EZConfig (additionalKeysP, checkKeymap, mkKeymap)
+import XMonad.Util.EZConfig
+  ( additionalKeysP,
+    checkKeymap,
+    mkKeymap,
+  )
 import XMonad.Util.NamedScratchpad
-import XMonad.Util.Run (runProcessWithInput, safeSpawn, spawnPipe)
+import XMonad.Util.Run
+  ( runProcessWithInput,
+    safeSpawn,
+    spawnPipe,
+  )
 import XMonad.Util.SpawnOnce
 
 -- doom-outrun-electric
-colorBack = "#0c0a20" --bg
+colorBack = "#0c0a20" -- bg
 
-colorFore = "#f2f3f7" --fg
+colorFore = "#f2f3f7" -- fg
 
-color0 = "#1f1147" --base1
+color0 = "#1f1147" -- base1
 
-color1 = "#e61f44" --red
+color1 = "#e61f44" -- red
 
-color2 = "#a7da1e" --green
+color2 = "#a7da1e" -- green
 
-color3 = "#cf433e" --orange
+color3 = "#cf433e" -- orange
 
-color4 = "#1ea8fc" --blue
+color4 = "#1ea8fc" -- blue
 
-color5 = "#ff2afc" --magenta
+color5 = "#ff2afc" -- magenta
 
-color6 = "#204052" --dark-cyan
+color6 = "#204052" -- dark-cyan
 
-color7 = "#110d26" --base2
+color7 = "#110d26" -- base2
 
-color8 = "#ba45a3" --base5
+color8 = "#ba45a3" -- base5
 
-color9 = "#cf433e" --orange
+color9 = "#cf433e" -- orange
 
-color10 = "#a875ff" --teal
+color10 = "#a875ff" -- teal
 
-color11 = "#ffd400" --yellow
+color11 = "#ffd400" -- yellow
 
-color12 = "#3f88ad" --dark-blue
+color12 = "#3f88ad" -- dark-blue
 
-color13 = "#df85ff" --violet
+color13 = "#df85ff" -- violet
 
-color14 = "#42c6ff" --cyan
+color14 = "#42c6ff" -- cyan
 
-color15 = "#919ad9" --base8
+color15 = "#919ad9" -- base8
 
 myFont :: String
-myFont = "xft:Inconsolata Nerd Font Mono:regular:size=9:antialias=true:hinting=true"
+myFont =
+  "xft:Inconsolata Nerd Font Mono:regular:size=9:antialias=true:hinting=true"
 
 myModMask :: KeyMask
 myModMask = mod4Mask
@@ -110,23 +167,36 @@ myFocusColor :: String
 myFocusColor = color14 -- Border color of focused windows
 
 windowCount :: X (Maybe String)
-windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
+windowCount =
+  gets $
+    Just
+      . show
+      . length
+      . W.integrate'
+      . W.stack
+      . W.workspace
+      . W.current
+      . windowset
 
 colorTrayer :: String
-colorTrayer = "--tint " ++ "0x" ++ tail colorBack
+colorTrayer = "--tint 0x" ++ tail colorBack
 
 -- Status Bar
 myPP =
   def
     { ppCurrent =
         xmobarColor color5 ""
-          . wrap ("<box type=Bottom width=2 mb=2 color=" ++ color5 ++ ">") "</box>",
+          . wrap
+            ("<box type=Bottom width=2 mb=2 color=" ++ color5 ++ ">")
+            "</box>",
       -- Visible but not current workspace
       ppVisible = xmobarColor color5 "",
       -- Hidden workspace
       ppHidden =
         xmobarColor color4 ""
-          . wrap ("<box type=Top width=2 mt=2 color=" ++ color4 ++ ">") "</box>",
+          . wrap
+            ("<box type=Top width=2 mt=2 color=" ++ color4 ++ ">")
+            "</box>",
       -- Hidden workspaces (no windows)
       ppHiddenNoWindows = xmobarColor color4 "",
       -- Title of active window
@@ -143,43 +213,45 @@ myPP =
 
 barSpawner :: ScreenId -> IO StatusBarConfig
 barSpawner 0 = pure $ statusBarProp "xmobar -x 0 ~/.config/xmobar/xmobarrc" $ pure myPP
-barSpawner 1 = pure $ statusBarProp "xmobar -x 1 ~/.config/xmobar/xmobarrc" $ pure myPP
-barSpawner 2 = pure $ statusBarProp "xmobar -x 2 ~/.config/xmobar/xmobarrc" $ pure myPP
+barSpawner _ = mempty
 
 myStartupHook :: X ()
 myStartupHook = do
-  return () >> checkKeymap myConfig myKeymap
-  spawnOnce "lxsession &"
-  spawnOnce "picom &"
-  spawnOnce "xscreensaver -no-splash"
-  spawnOnce "nm-applet &"
-  spawnOnce "volumeicon &"
   spawnOnce $ "killall trayer; trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor primary --transparent true --alpha 0 " ++ colorTrayer ++ " --height 30 &"
 
-myScratchPads :: [NamedScratchpad]
-myScratchPads =
+myScratchpads :: [NamedScratchpad]
+myScratchpads =
   [ NS "terminal" spawnTerm findTerm manageTerm,
     NS "calculator" spawnCalc findCalc manageCalc
   ]
   where
-    spawnTerm = myTerminal ++ " -t scratchpad"
-    findTerm = title =? "scratchpad"
+    spawnTerm = myTerminal ++ "--class=Scratchpad-Terminal"
+    findTerm = className =? "Scratchpad-Terminal"
     manageTerm = customFloating $ W.RationalRect l t w h
       where
-        l = 0.95 - w; t = 0.95 - h; w = 0.9; h = 0.9
+        l = 0.95 - w
+        t = 0.95 - h
+        w = 0.9
+        h = 0.9
     spawnCalc = "qalculate-gtk"
     findCalc = className =? "Qalculate-gtk"
     manageCalc = customFloating $ W.RationalRect l t w h
       where
-        l = 0.7 - w; t = 0.75 - h; w = 0.4; h = 0.5
+        l = 0.7 - w
+        t = 0.75 - h
+        w = 0.4
+        h = 0.5
 
---Makes setting the spacingRaw simpler to write. The spacingRaw module adds a configurable amount of space around windows.
-mySpacing :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
+-- Makes setting the spacingRaw simpler to write.
+-- The spacingRaw module adds a configurable amount of space around windows.
+mySpacing ::
+  Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
 mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
 
 -- Below is a variation of the above except no borders are applied
 -- if fewer than two windows. So a single window has no gaps.
-mySpacing' :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
+mySpacing' ::
+  Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
 mySpacing' i = spacingRaw True (Border i i i i) True (Border i i i i) True
 
 tall =
@@ -190,9 +262,8 @@ tall =
           subLayout [] (smartBorders Simplest) $
             limitWindows 6 $
               mySpacing' 6 $
-                Mirror $
-                  Mirror $
-                    ResizableTall 1 (3 / 100) (1 / 2) []
+                reflectHoriz $
+                  ResizableTall 1 (3 / 100) (1 / 2) []
 
 monocle =
   renamed [Replace "monocle"] $
@@ -210,8 +281,7 @@ grid =
           subLayout [] (smartBorders Simplest) $
             limitWindows 12 $
               mySpacing' 6 $
-                mkToggle (single MIRROR) $
-                  Grid (16 / 10)
+                Grid (16 / 10)
 
 threeCol =
   renamed [Replace "threeCol"] $
@@ -232,11 +302,8 @@ threeRow =
               Mirror $
                 ThreeCol 1 (3 / 100) (1 / 2)
 
-tabs =
-  renamed [Replace "tabs"] $
-    tabbed shrinkText myTabTheme
+tabs = renamed [Replace "tabs"] $ tabbed shrinkText myTabTheme
 
--- setting colors for tabs layout and tabs sublayout.
 myTabTheme =
   def
     { fontName = myFont,
@@ -248,7 +315,6 @@ myTabTheme =
       inactiveTextColor = colorFore
     }
 
--- Theme for showWName which prints current workspace when you change workspaces.
 myShowWNameTheme :: SWNConfig
 myShowWNameTheme =
   def
@@ -259,7 +325,13 @@ myShowWNameTheme =
     }
 
 -- The layout hook
-myLayoutHook = avoidStruts $ mouseResize $ windowArrange $ mkToggle (NBFULL ?? NOBORDERS ?? EOT) myDefaultLayout
+myLayoutHook =
+  avoidStruts $
+    mouseResize $
+      windowArrange $
+        mkToggle
+          (NBFULL ?? NOBORDERS ?? EOT)
+          myDefaultLayout
   where
     myDefaultLayout =
       withBorder myBorderWidth tall
@@ -269,7 +341,17 @@ myLayoutHook = avoidStruts $ mouseResize $ windowArrange $ mkToggle (NBFULL ?? N
         ||| threeCol
         ||| threeRow
 
-myWorkspaces = [" sys ", " doc ", " www ", " dev ", " cht ", " vms ", " mus ", " vid ", " gfx "]
+myWorkspaces =
+  [ " sys ",
+    " doc ",
+    " www ",
+    " dev ",
+    " cht ",
+    " emu ",
+    " mus ",
+    " vid ",
+    " gfx "
+  ]
 
 myManageHook =
   composeAll
@@ -298,7 +380,7 @@ myManageHook =
       className =? "Inkscape" --> doShift (myWorkspaces !! 8),
       isFullscreen --> doFullFloat
     ]
-    <+> namedScratchpadManageHook myScratchPads
+    <+> namedScratchpadManageHook myScratchpads
 
 -- START_KEYS
 myKeymap :: [(String, X ())]
@@ -307,61 +389,66 @@ myKeymap =
     ("M-C-r", spawn "xmonad --recompile"),
     ("M-S-r", spawn "xmonad --restart"),
     ("M-S-x", io exitSuccess),
-    -- KB_GROUP Run Prompt
+    -- KB_GROUP Run prompt
     ("M-S-<Return>", spawn myRunPrompt),
-    ("M-C-<Return>", spawn myRunPrompt), -- Workaround for when M-S-<Return> won't register <Return>
-
     -- KB_GROUP Commonly used programs
     ("M-<Return>", spawn myTerminal),
     ("M-b", spawn myBrowser),
-    ("M-M1-h", spawn (myTerminal ++ " -e htop")),
     -- KB_GROUP Kill windows
-    ("M-S-q", kill1), -- Kill the currently focused client
-    ("M-S-c", killAll), -- Kill all windows on current workspace
-
+    ("M-S-q", kill1),
+    ("M-S-c", killAll),
     -- KB_GROUP Workspaces
-    ("M-.", nextScreen), -- Switch focus to next monitor
-    ("M-,", prevScreen), -- Switch focus to prev monitor
-    ("M-S-<KP_Add>", shiftTo Next nonNSP >> moveTo Next nonNSP), -- Shifts focused window to next ws
-    ("M-S-<KP_Subtract>", shiftTo Prev nonNSP >> moveTo Prev nonNSP), -- Shifts focused window to prev ws
-
-    -- KB_GROUP Floating windows
-    ("M-t", withFocused $ windows . W.sink), -- Push floating window back to tile
-    ("M-S-t", sinkAll), -- Push ALL floating windows to tile
-
+    ("M-.", nextScreen),
+    -- Switch focus to next monitor
+    ("M-,", prevScreen),
+    -- Switch focus to prev monitor
+    ("M-S-<KP_Add>", shiftTo Next nonNSP >> moveTo Next nonNSP),
+    -- Shift to next ws
+    ("M-S-<KP_Subtract>", shiftTo Prev nonNSP >> moveTo Prev nonNSP),
+    -- Shift to prev ws
+    -- KB_GROUP Floating
+    ("M-t", withFocused $ windows . W.sink),
+    -- Push to tile
+    ("M-S-t", sinkAll),
+    -- Push all to tiles
     -- KB_GROUP Increase/decrease spacing (gaps)
-    ("C-M1-m", decScreenSpacing 2), -- Decrease screen spacing
-    ("C-M1-n", decWindowSpacing 2), -- Decrease window spacing
-    ("C-M1-e", incWindowSpacing 2), -- Increase window spacing
-    ("C-M1-i", incScreenSpacing 2), -- Increase screen spacing
-
-    -- KB_GROUP Windows navigation
-    ("M-m", windows W.focusMaster), -- Move focus to the master window
-    ("M-n", windows W.focusDown), -- Move focus to the next window
-    ("M-e", windows W.focusUp), -- Move focus to the prev window
-    ("M-i", windows W.swapMaster), -- Swap the focused window and the master window
-    ("M-S-n", windows W.swapDown), -- Swap focused window with next window
-    ("M-S-e", windows W.swapUp), -- Swap focused window with prev window
-    ("M-<Backspace>", promote), -- Moves focused window to master, others maintain order
-    ("M-S-<Tab>", rotSlavesDown), -- Rotate all windows except master and keep focus in place
-    ("M-C-<Tab>", rotAllDown), -- Rotate all the windows in the current stack
-
+    ("C-M1-m", decScreenSpacing 2),
+    -- Decrease screen gaps
+    ("C-M1-n", decWindowSpacing 2),
+    -- Decrease window gaps
+    ("C-M1-e", incWindowSpacing 2),
+    -- Increase window gaps
+    ("C-M1-i", incScreenSpacing 2),
+    -- Increase screen gaps
+    -- KB_GROUP Navigation
+    ("M-m", windows W.focusMaster),
+    ("M-n", windows W.focusDown),
+    ("M-e", windows W.focusUp),
+    -- KB_GROUP Manipulation
+    ("M-<Backspace>", promote),
+    ("M-i", windows W.swapMaster),
+    ("M-S-n", windows W.swapDown),
+    ("M-S-e", windows W.swapUp),
+    ("M-S-<Tab>", rotSlavesDown),
+    ("M-C-<Tab>", rotAllDown),
     -- KB_GROUP Layouts
-    ("M-<Tab>", sendMessage NextLayout), -- Switch to next layout
-    ("M-<Space>", sendMessage (MT.Toggle NBFULL) >> sendMessage ToggleStruts), -- Toggles noborder/full
-
-    -- KB_GROUP Increase/decrease windows in the master pane or the stack
-    ("M-S-<Up>", sendMessage (IncMasterN 1)), -- Increase # of clients master pane
-    ("M-S-<Down>", sendMessage (IncMasterN (-1))), -- Decrease # of clients master pane
-    ("M-C-<Up>", increaseLimit), -- Increase # of windows
-    ("M-C-<Down>", decreaseLimit), -- Decrease # of windows
-
+    ("M-<Tab>", sendMessage NextLayout),
+    ("M-<Space>", sendMessage (MT.Toggle NBFULL) >> sendMessage ToggleStruts),
+    -- Fullscreen
+    -- KB_GROUP Window limits
+    ("M-S-<Up>", sendMessage (IncMasterN 1)),
+    -- Increase # in master
+    ("M-S-<Down>", sendMessage (IncMasterN (-1))),
+    -- Decrease # in master
+    ("M-C-<Up>", increaseLimit),
+    -- Increase #
+    ("M-C-<Down>", decreaseLimit),
+    -- Decrease #
     -- KB_GROUP Window resizing
-    ("M-h", sendMessage Shrink), -- Shrink horiz window width
-    ("M-l", sendMessage Expand), -- Expand horiz window width
-    ("M-M1-n", sendMessage MirrorShrink), -- Shrink vert window width
-    ("M-M1-e", sendMessage MirrorExpand), -- Expand vert window width
-
+    ("M-h", sendMessage Shrink),
+    ("M-l", sendMessage Expand),
+    ("M-M1-n", sendMessage MirrorShrink),
+    ("M-M1-e", sendMessage MirrorExpand),
     -- KB_GROUP Sublayouts
     -- This is used to push windows to tabbed sublayouts, or pull them out of it.
     ("M-C-m", sendMessage $ pullGroup L),
@@ -370,29 +457,23 @@ myKeymap =
     ("M-C-i", sendMessage $ pullGroup D),
     ("M-C-?", withFocused (sendMessage . MergeAll)),
     ("M-C-/", withFocused (sendMessage . UnMergeAll)),
-    ("M-C-.", onGroup W.focusUp'), -- Switch focus to next tab
-    ("M-C-,", onGroup W.focusDown'), -- Switch focus to prev tab
-
+    ("M-C-.", onGroup W.focusUp'),
+    ("M-C-,", onGroup W.focusDown'),
     -- KB_GROUP Scratchpads
-    -- Toggle show/hide these programs.  They run on a hidden workspace.
-    -- When you toggle them to show, it brings them to your current workspace.
-    -- Toggle them to hide and it sends them back to hidden workspace (NSP).
-    ("M-s t", namedScratchpadAction myScratchPads "terminal"),
-    ("M-s c", namedScratchpadAction myScratchPads "calculator"),
+    ("M-s t", namedScratchpadAction myScratchpads "terminal"),
+    ("M-s c", namedScratchpadAction myScratchpads "calculator"),
     -- KB_GROUP Emacs
-    ("M-a a", spawn myEmacs), -- emacs
-    ("M-a e", spawn $ myEmacs ++ "--eval '(eshell)'"), -- eshell
-    ("M-a f", spawn $ myEmacs ++ "--eval '(elfeed)'"), -- elfeed
-    ("M-a w", spawn $ myEmacs ++ "--eval '(eww)'"), -- emacs web wowser
-    ("M-a i", spawn $ myEmacs ++ "--eval '(circe)'"), -- emacs irc client
-
+    ("M-a a", spawn myEmacs),
+    ("M-a e", spawn $ myEmacs ++ "--eval '(eshell)'"),
+    ("M-a f", spawn $ myEmacs ++ "--eval '(elfeed)'"),
+    ("M-a w", spawn $ myEmacs ++ "--eval '(eww)'"),
+    ("M-a i", spawn $ myEmacs ++ "--eval '(circe)'"),
     -- KB_GROUP XF86
     ("<XF86AudioMute>", spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle"),
     ("<XF86AudioMicMute>", spawn "pactl set-source-mute @DEFAULT_SOURCE@ toggle")
   ]
   where
     -- The following lines are needed for named scratchpads.
-
     nonNSP = WSIs (return (\ws -> W.tag ws /= "NSP"))
     nonEmptyNonNSP = WSIs (return (\ws -> isJust (W.stack ws) && W.tag ws /= "NSP"))
 
