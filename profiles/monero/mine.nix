8c2e83daac03e70923f01a1e300bc03a68cf5313
Lord-Valen <lord_valen@protonmail.com>
2022-11-30 22:39:07 -0500

    :rocket: `feat` Add p2pool module

--- profiles/monero/mine.nix
+++ profiles/monero/mine.nix
@@ -6,11 +6,9 @@
 }: {
   imports = [./common.nix];
   services = {
-    monero = {
-      mining = {
-        enable = true;
-        address = "44JVCqQB9CvNn1HJcL9SfyBdGLbFk1dApXV1DWBrsoi7GJyfJaa6Vjbi2Y9SCM5wmBTMxbamAm3sjRpEJMMy4R9EPvYWHQe";
-      };
+    p2pool = {
+      enable = true;
+      address = "44JVCqQB9CvNn1HJcL9SfyBdGLbFk1dApXV1DWBrsoi7GJyfJaa6Vjbi2Y9SCM5wmBTMxbamAm3sjRpEJMMy4R9EPvYWHQe";
     };
   };
 }
