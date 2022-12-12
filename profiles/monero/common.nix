c3e35c6c6c83f26099d0651bd0965063f84b0b55
Lord-Valen <lord_valen@protonmail.com>
2022-12-12 15:07:12 -0500

    :gear: `value` Add priority nodes

--- profiles/monero/common.nix
+++ profiles/monero/common.nix
@@ -6,5 +6,13 @@
 }: {
   environment.systemPackages = with pkgs; [monero-gui];
 
-  services = {monero = {enable = true;};};
+  services.monero = {
+    enable = true;
+    priorityNodes = [
+      "node.moneroworld.com:18080"
+      "opennode.xmr-tw.org:18080"
+      "node.supportxmr.com:18080"
+      "nodes.hashvault.pro:18080"
+    ];
+  };
 }
