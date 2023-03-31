f2f88331b36ff4515b968d68a183a2e2cb4190d9
Lord-Valen <lord_valen@protonmail.com>
2023-03-31 14:05:02 -0400

    feat: add forge usernames

--- comb/lord-valen/home/profiles/git/valen.nix
+++ comb/lord-valen/home/profiles/git/valen.nix
@@ -1,12 +1,12 @@
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
   programs.git = {
     userName = "Lord-Valen";
     userEmail = "lord_valen@protonmail.com";
+
+    extraConfig = {
+      github.user = "Lord-Valen";
+      gitlab.user = "Lord-Valen";
+    };
 
     signing = {
       key = null;
