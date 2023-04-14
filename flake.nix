97e0d624091a6e57ee517f33173e16cad38c5035
Lord-Valen <lord_valen@protonmail.com>
2023-04-14 17:24:19 -0400

    chore: use colmena cachix

--- flake.nix
+++ flake.nix
@@ -11,12 +11,15 @@
       "https://ezkea.cachix.org"
       # hyprland
       "https://hyprland.cachix.org"
+      # colmena
+      "https://colmena.cachix.org"
     ];
 
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
     ];
   };
 
@@ -65,7 +68,6 @@
 
     colmena = {
       url = "github:zhaofengli/colmena";
-      inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-compat.follows = "blank";
     };
 
