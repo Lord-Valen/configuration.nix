8ae71648458db0dd63f11e0d66ffec1a002b8d3b
Lord-Valen <lord_valen@protonmail.com>
2023-04-15 17:10:52 -0400

    feat: source monitor.conf

--- comb/lord-valen/home/profiles/wayland/hyprland/default.nix
+++ comb/lord-valen/home/profiles/wayland/hyprland/default.nix
@@ -1,5 +1,10 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   home.packages = with pkgs; [kitty wofi waylock swww eww-wayland];
 
   xdg.configFile."hypr/hyprland.conf".source = ./hyprland.conf;
+  xdg.configFile."hypr/monitor.conf".text = lib.mkDefault ''monitor=,preferred,auto,auto'';
 }
