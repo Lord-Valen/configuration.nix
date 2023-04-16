a74604cb5be1e0b90bc36a29e5067ddcc2a4ea3b
Lord-Valen <lord_valen@protonmail.com>
2023-04-16 03:22:22 -0400

    fix: don't check config

--- comb/lord-valen/home/profiles/colemak.nix
+++ comb/lord-valen/home/profiles/colemak.nix
@@ -1,12 +1,8 @@
-{
-  lib,
-  config,
-  ...
-}: {
-  xdg.configFile."keyboard.conf".text = lib.mkIf (config.home.xdg.configFile."hypr/hyprland.conf" != null) (lib.mkForce ''
+{lib, ...}: {
+  xdg.configFile."hypr/keyboard.conf".text = lib.mkForce ''
     input {
       kb_layout = us
       kb_variant = colemak_dh
     }
-  '');
+  '';
 }
