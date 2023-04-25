f9e3262253fb0c35c694459bd28a3160a0b11f30
Lord-Valen <lord_valen@protonmail.com>
2023-04-24 20:42:56 -0400

    feat: open firewall

--- comb/lord-valen/nixos/profiles/dev/npm.nix
+++ comb/lord-valen/nixos/profiles/dev/npm.nix
@@ -1,9 +1,10 @@
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
   # Enables us to use `npm link` without running into permissions issues
   programs.npm.enable = true;
+
+  # Host test sites
+  networking.firewall = {
+    allowedTCPPorts = [3000];
+    allowedUDPPorts = [3000];
+  };
 }
