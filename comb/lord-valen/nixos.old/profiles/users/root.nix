cb2ea8a18c0861883b9354b27ab9ed917e45079e
Lord-Valen <lord_valen@protonmail.com>
2023-05-12 21:15:13 -0400

    fixup! feat: add ssh authorization

--- comb/lord-valen/nixos.old/profiles/users/root.nix
+++ comb/lord-valen/nixos.old/profiles/users/root.nix
@@ -1,9 +1,9 @@
 {...}: {
   users.users.root = {
     initialHashedPassword = "$6$rZhNhLxPNJx.lRBn$lXAcMr7CdFgjRcN4ZMlEai2QYWMoawm6pMKrd9oFHXgWks9KBkP3p7Afj/Djj1LnCDyXbLNT5IfVNjDEUzk1p0";
-    openssh.enable = true;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH6sqfUwKC9nKlRbH9laeIBJjn9fDqIH57JsLFbMDAKh"
     ];
   };
+  services.openssh.enable = true;
 }
