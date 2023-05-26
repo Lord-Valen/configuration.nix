97b8d97546b39ad6f6671cfc658a796ae759342c
Lord-Valen <lord_valen@protonmail.com>
2023-05-26 18:32:30 -0400

    refactor: use renamed option

--- comb/lord-valen/nixosProfiles/games/programs.nix
+++ comb/lord-valen/nixosProfiles/games/programs.nix
@@ -3,7 +3,7 @@
   cell,
 }: {
   _imports = [inputs.aagl-gtk-on-nix.nixosModules.default];
-  an-anime-game-launcher.enable = true;
+  anime-game-launcher.enable = true;
   steam = {
     enable = true;
     remotePlay.openFirewall = true;
