3e9bcc2eb9314ad16d4a3783910e5c59312afd1f
Lord-Valen <lord_valen@protonmail.com>
2023-05-26 21:46:59 -0400

    fix: don't disable openFirewall

--- comb/lord-valen/nixosProfiles/core/services.nix
+++ comb/lord-valen/nixosProfiles/core/services.nix
@@ -1,15 +1,4 @@
 {
-  inputs,
-  cell,
-}: let
-  inherit (cell) lib;
-in {
-  # For rage encryption, all hosts need a ssh key pair
-  openssh = {
-    enable = true;
-    openFirewall = lib.mkDefault false;
-  };
-
-  # Service that makes Out of Memory Killer more effective
+  openssh.enable = true;
   earlyoom.enable = true;
 }
