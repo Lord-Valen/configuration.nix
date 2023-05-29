3d061cc596024bd1bc5b3c804df5b47e12dcdad7
Lord-Valen <lord_valen@protonmail.com>
2023-05-28 21:29:31 -0400

    chore: switch hm branch

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
