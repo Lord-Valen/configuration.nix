75debcc5813877cec031dd9962fc0f0760eec19e
Lord-Valen <lord_valen@protonmail.com>
2023-05-30 18:25:15 -0400

    feat: add xdg portal

--- comb/lord-valen/homeProfiles/hyprland/xdg.nix
+++ comb/lord-valen/homeProfiles/hyprland/xdg.nix
@@ -1,7 +1,9 @@
 {
   inputs,
   cell,
-}: {
+}: let
+  inherit (inputs) nixpkgs;
+in {
   configFile."hypr/hyprland.conf".source = ./_hyprland.conf;
   configFile."hypr/monitor.conf".text = cell.lib.mkDefault ''monitor=,preferred,auto,auto'';
   configFile."hypr/keyboard.conf".text = cell.lib.mkDefault ''
@@ -9,4 +11,9 @@
       kb_layout = us
     }
   '';
+
+  portal = {
+    enable = true;
+    extraPortals = with nixpkgs; [xdg-desktop-portal-hyprland];
+  };
 }
