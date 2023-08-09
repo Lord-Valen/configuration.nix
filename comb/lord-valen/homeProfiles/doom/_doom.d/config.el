44d8fdb8e7ac9bd6bde94548d8848cd04ef0c63b
Lord-Valen <lord_valen@protonmail.com>
2023-08-09 15:02:51 -0400

    feat: increase doom font size

--- comb/lord-valen/homeProfiles/doom/_doom.d/config.el
+++ comb/lord-valen/homeProfiles/doom/_doom.d/config.el
@@ -5,9 +5,9 @@
 
 (setq shell-file-name "nu")
 
-(setq doom-font (font-spec :family "Fira Code" :size 13)
-      doom-variable-pitch-font (font-spec :family "Fira Code")
-      doom-big-font (font-spec :family "Fira Code" :size 18)
+(setq doom-font (font-spec :family "Fira Code" :size 16)
+      doom-variable-pitch-font (font-spec :family "Fira Code" :size 16)
+      doom-big-font (font-spec :family "Fira Code" :size 24)
       doom-unicode-font doom-font)
 
 (custom-set-faces!
