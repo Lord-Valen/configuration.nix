bb476ece92443d2e0c23051db79ead55c4ff139a
Lord-Valen <lord_valen@protonmail.com>
2023-08-10 14:13:41 -0400

    feat: use melpa nushell-mode

--- comb/lord-valen/homeProfiles/doom/default.nix
+++ comb/lord-valen/homeProfiles/doom/default.nix
@@ -16,18 +16,6 @@ in {
       emacsPackages.vterm
     ];
     emacsPackagesOverlay = self: super: {
-      nushell-mode = self.trivialBuild {
-        pname = "nushell-mode";
-        ename = "nushell-mode";
-        version = "0.1.0";
-        buildInputs = [];
-        src = inputs.nixpkgs.fetchFromGitHub {
-          owner = "azzamsa";
-          repo = "emacs-nushell";
-          rev = "5ffe4382fef4bad87ebc1fd5fb9787d40d6ea128";
-          sha256 = "07mfr8skx9xncmdxf95fhxrwdys170606pb411layd8wzn0pfm7i";
-        };
-      };
     };
   };
 
