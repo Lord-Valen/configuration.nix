d34eeff3271169a6c0b0339f79d36c1760c74e3f
Lord-Valen <lord_valen@protonmail.com>
2023-10-14 19:34:01 -0400

    feat: add gamescope

--- comb/lord-valen/nixosProfiles/games/programs.nix
+++ comb/lord-valen/nixosProfiles/games/programs.nix
@@ -1,12 +1,23 @@
 {
   inputs,
   cell,
-}: {
+}: let
+  inherit (inputs) nixpkgs;
+in {
   _imports = [inputs.aagl-gtk-on-nix.nixosModules.default];
   anime-game-launcher.enable = true;
   steam = {
     enable = true;
+    package = nixpkgs.steam.override {
+      extraPkgs = pkgs:
+        with pkgs; [
+          keyutils
+          libkrb5
+        ];
+    };
     remotePlay.openFirewall = true;
+    gamescopeSession.enable = true;
   };
+  gamescope.enable = true;
   gamemode.enable = true;
 }
