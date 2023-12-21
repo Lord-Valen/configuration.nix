641cdee9c597e631eb01d0676abb9e86c3ae9a73
Lord-Valen <lord_valen@protonmail.com>
2023-12-21 18:58:11 -0500

    fix: set heracles not to be an introducer

--- comb/lord-valen/nixosProfiles/syncthing/default.nix
+++ comb/lord-valen/nixosProfiles/syncthing/default.nix
@@ -21,7 +21,6 @@ in {
         };
         "Heracles" = lib.mkIf (hostName != "heracles") {
           id = "P24GAXD-4KBICX2-ZT4UP66-TF4EIOK-M7MQLQK-QW4QNWC-ALONBBQ-QGVICQB";
-          introducer = true;
         };
         "Theseus" = lib.mkIf (hostName != "theseus") {
           id = "JFJTOJX-S6ITXDO-PJ5CEYI-ZTCAUFJ-7J22EFK-H7VX4AA-KJZ3QLW-XED4WQG";
