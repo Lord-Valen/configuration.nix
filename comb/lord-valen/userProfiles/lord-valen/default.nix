c88c501c8182086bc09657ed41e53b8cdaa36edc
Lord-Valen <lord_valen@protonmail.com>
2023-12-21 19:06:47 -0500

    feat: allow ssh keys on lord-valen

--- comb/lord-valen/userProfiles/lord-valen/default.nix
+++ comb/lord-valen/userProfiles/lord-valen/default.nix
@@ -10,6 +10,10 @@ in {
     isNormalUser = true;
     createHome = true;
     extraGroups = ["adbusers" "libvirtd" "networkmanager" "wheel" "config.services.kubo.group"];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJlTDo77mX1eDjo5o44C9pvIt+8nOptLVJQoGr1/Ilgl" #cardno:25_313_700
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICpUIUwaDNzvZJpkhhsK/yN1DaMCqhpmDFILhXG1kfOr" #cardno:20_624_908
+    ];
     shell = nixpkgs.nushell;
   };
 }
