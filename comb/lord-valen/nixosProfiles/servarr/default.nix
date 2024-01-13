9b1d173556c3b328a5a0f5d01afe6b56a1bc6cdb
Lord-Valen <lord_valen@protonmail.com>
2024-01-12 20:13:26 -0500

    feat: enable calibre-server

--- comb/lord-valen/nixosProfiles/servarr/default.nix
+++ comb/lord-valen/nixosProfiles/servarr/default.nix
@@ -1,11 +1,24 @@
 {
   inputs,
   cell,
+  config,
 }: let
   inherit (inputs) nixpkgs;
   inherit (nixpkgs) lib;
 in {
+  networking.firewall = {
+    allowedTCPPorts = [8080];
+  };
   services = {
+    calibre-server = {
+      enable = true;
+      auth = {
+        enable = true;
+        mode = "basic";
+        userDb = "/var/lib/calibre-server/users.sqlite";
+      };
+    };
+
     deluge = {
       enable = true;
       user = "servarr";
@@ -23,15 +36,15 @@ in {
       openFirewall = true;
     };
 
-    prowlarr = {
-      enable = true;
-      openFirewall = true;
-    };
-
     lidarr = {
       enable = true;
       user = "servarr";
       group = "servarr";
+      openFirewall = true;
+    };
+
+    prowlarr = {
+      enable = true;
       openFirewall = true;
     };
 
