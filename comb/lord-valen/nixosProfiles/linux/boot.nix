8253e88b05bd74cc769eb6edbe93ee8ce0ebb99a
Lord-Valen <lord_valen@protonmail.com>
2024-01-20 00:44:23 -0500

    feat: set sysrq value

---
+++ comb/lord-valen/nixosProfiles/linux/boot.nix
@@ -0,0 +1,9 @@
+{
+  inputs,
+  cell,
+}: let
+  inherit (inputs) nixpkgs;
+in {
+  kernelPackages = nixpkgs.linuxKernel.packageAliases.linux_latest;
+  kernel.sysctl.kernel.sysrq = 244;
+}
--- comb/lord-valen/nixosProfiles/linux/default.nix
+++
@@ -1,8 +0,0 @@
-{
-  inputs,
-  cell,
-}: let
-  inherit (inputs) nixpkgs;
-in {
-  boot.kernelPackages = nixpkgs.linuxKernel.packageAliases.linux_latest;
-}
