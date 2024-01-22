fcf24939231dce3be0d5d099864dac7155e4f44e
Lord-Valen <lord_valen@protonmail.com>
2024-01-22 16:02:35 -0500

    fix: add lvHeracles

--- comb/lord-valen/nixosProfiles/syncthing/default.nix
+++ comb/lord-valen/nixosProfiles/syncthing/default.nix
@@ -25,6 +25,9 @@ in {
         "Heracles" = lib.mkIf (hostName != "heracles") {
           id = "P24GAXD-4KBICX2-ZT4UP66-TF4EIOK-M7MQLQK-QW4QNWC-ALONBBQ-QGVICQB";
         };
+        "lvHeracles" = lib.mkIf (hostName != "heracles") {
+          id = "PYBY33V-VYJN4YU-L67K3CX-2HNOZYA-UW3HXRW-Q6IRUEZ-UY6DBPO-JY47NQ6";
+        };
         "Theseus" = lib.mkIf (hostName != "theseus") {
           id = "JFJTOJX-S6ITXDO-PJ5CEYI-ZTCAUFJ-7J22EFK-H7VX4AA-KJZ3QLW-XED4WQG";
         };
