b5ad159c8c3080c62543c278add50121ef9107e7
Lord-Valen <lord_valen@protonmail.com>
2024-03-04 18:53:59 -0500

    fix: fix sdb label

--- comb/lord-valen/diskoConfigurations/heracles/disk/sdb/default.nix
+++ comb/lord-valen/diskoConfigurations/heracles/disk/sdb/default.nix
@@ -3,8 +3,10 @@
   type = "disk";
   content = {
     type = "gpt";
-    partitions.GAME = {
+    partitions.GAME = rec {
       name = "GAME";
+      label = name;
+      device = "/dev/disk/by-label/${label}";
       size = "100%";
       content = {
         type = "btrfs";
