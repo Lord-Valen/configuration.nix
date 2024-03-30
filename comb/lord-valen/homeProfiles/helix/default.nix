763d5c997afb8d37eb8dda1ddf4c6a4e19427cd3
Lord-Valen <lord_valen@protonmail.com>
2024-03-29 22:43:47 -0400

    feat: rewrite helix profile

--- comb/lord-valen/homeProfiles/helix/default.nix
+++ comb/lord-valen/homeProfiles/helix/default.nix
@@ -1,11 +1,34 @@
 {
+  inputs,
+  cell,
+  lib,
+  pkgs,
+}:
+{
   programs.helix = {
     enable = true;
-    langauages =
+    extraPackages = with pkgs; [
+      nodePackages.typescript-language-server
+      nodePackages.vscode-langservers-extracted
+      nodePackages.yaml-language-server
+      nodePackages.bash-language-server
+      nixd
+      taplo
+      rust-analyzer
+      haskell-language-server
+      clang-tools
+      marksman
+    ];
+    languages.language-server = {
+      nixd = {
+        command = lib.getExe pkgs.nixd;
+      };
+    };
+    languages.language =
       let
         prettier = {
-          command = "prettier";
-          args = "-w";
+          command = lib.getExe pkgs.nodePackages.prettier;
+          args = lib.cli.toGNUCommandLine { } { w = true; };
         };
         jsRoots = [
           "package.json"
@@ -21,15 +44,18 @@
           name = "javascript";
           roots = jsRoots;
           formatter = prettier;
+          auto-format = true;
         }
         {
           name = "typescript";
           roots = jsRoots;
           formatter = prettier;
+          auto-format = true;
         }
         {
           name = "markdown";
           formatter = prettier;
+          auto-format = true;
         }
         {
           name = "nix";
@@ -37,6 +63,8 @@
             "flake.nix"
             "flake.lock"
           ];
+          auto-format = true;
+          language-servers = [ "nixd" ];
         }
       ];
   };
