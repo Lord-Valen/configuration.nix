4149711d9ff93636fe420ceb4c704412600f8c96
Lord-Valen <lord_valen@protonmail.com>
2024-04-27 00:59:44 -0400

    feat: slim down retroarch

--- comb/lord-valen/nixosProfiles/games/services.nix
+++ comb/lord-valen/nixosProfiles/games/services.nix
@@ -13,6 +13,18 @@
   };
   xserver.desktopManager.retroarch = {
     enable = true;
-    package = pkgs.retroarchFull;
+    package = pkgs.retroarch.override {
+      cores = with pkgs.libretro; [
+        scummvm
+        beetle-psx
+        ppsspp
+        pcsx2
+        mgba
+        mesen
+        bsnes
+        mupen64plus
+        blastem
+      ];
+    };
   };
 }
