db9b90b7224e7058a0fc8aa77354d1faeb829fa1
Lord-Valen <lord_valen@protonmail.com>
2024-05-10 22:03:54 -0400

    fix: fix gpg-agent

--- comb/lord-valen/nixosProfiles/gpg/default.nix
+++ comb/lord-valen/nixosProfiles/gpg/default.nix
@@ -2,9 +2,24 @@
   inputs,
   cell,
   pkgs,
+  lib,
 }:
 {
   hardware.gpgSmartcards.enable = true;
+  # TODO: Remove after https://nixpk.gs/pr-tracker.html?pr=308884 hits unstable
+  programs.gnupg.package = pkgs.gnupg.override {
+    pcsclite = pkgs.pcsclite.overrideAttrs (old: {
+      postPatch =
+        old.postPatch
+        + (lib.optionalString
+          (!(lib.strings.hasInfix ''--replace-fail "libpcsclite_real.so.1"'' old.postPatch))
+          ''
+            substituteInPlace src/libredirect.c src/spy/libpcscspy.c \
+              --replace-fail "libpcsclite_real.so.1" "$lib/lib/libpcsclite_real.so.1"
+          ''
+        );
+    });
+  };
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
