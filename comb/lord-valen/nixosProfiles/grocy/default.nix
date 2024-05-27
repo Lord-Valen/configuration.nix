4b4da7ee75f3b9ab2817085a05023c54716d4231
Lord-Valen <lord_valen@protonmail.com>
2024-05-27 18:09:02 -0400

    fix: open port 80; set hostName

--- comb/lord-valen/nixosProfiles/grocy/default.nix
+++ comb/lord-valen/nixosProfiles/grocy/default.nix
@@ -1,7 +1,9 @@
 {
+  networking.firewall.allowedTCPPorts = [ 80 ];
+  networking.firewall.allowedUDPPorts = [ 80 ];
   services.grocy = {
     enable = true;
-    hostName = "grocy";
+    hostName = "grocy.home *.grocy.home";
     nginx.enableSSL = false; # TODO
     settings = {
       currency = "CAD";
