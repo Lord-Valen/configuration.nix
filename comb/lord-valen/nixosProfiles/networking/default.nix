44b25066da98f1d9e795956a43849b15639804f8
Lord-Valen <lord_valen@protonmail.com>
2025-01-13 15:02:54 -0500

    feat: use default networking backend

--- comb/lord-valen/nixosProfiles/networking/default.nix
+++ comb/lord-valen/nixosProfiles/networking/default.nix
@@ -6,10 +6,9 @@
 {
   networking = {
     enableIPv6 = true;
-    wireless.iwd.enable = true;
     networkmanager = {
       enable = true;
-      wifi.backend = "iwd";
+      # wifi.backend = "iwd";
     };
   };
 
