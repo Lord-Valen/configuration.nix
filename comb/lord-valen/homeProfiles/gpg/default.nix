5eab3130491935cabd50c9a161130ebbedcf3903
Lord-Valen <lord_valen@protonmail.com>
2025-02-18 15:30:42 -0500

    chore: remove unnecessary overrides

--- comb/lord-valen/homeProfiles/gpg/default.nix
+++ comb/lord-valen/homeProfiles/gpg/default.nix
@@ -1,25 +1,5 @@
 {
-  inputs,
-  cell,
-  pkgs,
-  lib,
-}:
-{
   programs.gpg = {
-    # TODO: Remove after https://nixpk.gs/pr-tracker.html?pr=308884 hits unstable
-    package = pkgs.gnupg.override {
-      pcsclite = pkgs.pcsclite.overrideAttrs (old: {
-        postPatch =
-          old.postPatch
-          + (lib.optionalString
-            (!(lib.strings.hasInfix ''--replace-fail "libpcsclite_real.so.1"'' old.postPatch))
-            ''
-              substituteInPlace src/libredirect.c src/spy/libpcscspy.c \
-                --replace-fail "libpcsclite_real.so.1" "$lib/lib/libpcsclite_real.so.1"
-            ''
-          );
-      });
-    };
     enable = true;
     scdaemonSettings.disable-ccid = true;
     publicKeys = [
