464ce1c3ac8175fc2bc483b3713a545edbbb0a93
Lord-Valen <lord_valen@protonmail.com>
2025-07-14 01:34:57 -0400

    add rocm module

--- dendrite/flake.nix
+++ dendrite/flake.nix
@@ -53,6 +53,10 @@
   };
   nixConfig = {
     extra-experimental-features = "nix-command flakes pipe-operators";
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+    extra-trusted-substituters = [ "https://nix-community.cachix.org" ];
   };
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 }
