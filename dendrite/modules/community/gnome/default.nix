06f3d8ff554794f724552a02d8a1141bb42dfafe
Lord-Valen <lord_valen@protonmail.com>
2025-07-15 19:21:26 -0400

    add gnome module

---
+++ dendrite/modules/community/gnome/default.nix
@@ -0,0 +1,31 @@
+{
+  flake.modules.nixos.gnome =
+    { pkgs, ... }:
+    {
+      xdg.portal.enable = true;
+
+      services.libinput = {
+        enable = true;
+        touchpad.naturalScrolling = true;
+      };
+
+      services.xserver.desktopManager.gnome.enable = true;
+    };
+
+  flake.modules.home-manager.gnome =
+    { pkgs, ... }:
+    {
+      programs.gnome-shell = {
+        enable = true;
+        extensions =
+          with pkgs.gnomeExtensions;
+          map (package: { inherit package; }) [
+            paperwm
+            switcher
+            system-monitor
+            status-icons
+            lilypad
+          ];
+      };
+    };
+}
