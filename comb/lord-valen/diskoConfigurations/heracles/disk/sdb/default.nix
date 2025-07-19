60012129fa3703a4733f75572a3b2f4b2563c833
Lord-Valen <lord_valen@protonmail.com>
2025-07-18 20:40:10 -0400

    update sdb

--- comb/lord-valen/diskoConfigurations/heracles/disk/sdb/default.nix
+++ comb/lord-valen/diskoConfigurations/heracles/disk/sdb/default.nix
@@ -1,32 +1,25 @@
-{
+rec {
   device = "/dev/sdb";
   type = "disk";
+  name = "disk-sdb-GAME";
   content = {
-    type = "gpt";
-    partitions.GAME = rec {
-      name = "GAME";
-      label = name;
-      device = "/dev/disk/by-label/${label}";
-      size = "100%";
-      content = {
-        type = "btrfs";
-        extraArgs = [ "-f" ];
-        subvolumes = {
-          "@" = {
-            mountpoint = "/home/lord-valen/Games";
-            mountOptions = [
-              "noatime"
-              "compress=zstd"
-            ];
-          };
-          ".snapshots/@" = {
-            mountpoint = "/home/lord-valen/Games/.snapshots";
-            mountOptions = [
-              "noatime"
-              "compress=zstd"
-            ];
-          };
-        };
+    type = "btrfs";
+    device = "/dev/disk/by-label/${name}";
+    extraArgs = [ "-f" ];
+    subvolumes = {
+      "@" = {
+        mountpoint = "/home/lord-valen/Games";
+        mountOptions = [
+          "noatime"
+          "compress=zstd"
+        ];
+      };
+      ".snapshots/@" = {
+        mountpoint = "/home/lord-valen/Games/.snapshots";
+        mountOptions = [
+          "noatime"
+          "compress=zstd"
+        ];
       };
     };
   };
