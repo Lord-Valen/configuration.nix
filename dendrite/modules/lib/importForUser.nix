e05e7a664d9fd5795e877ad94d3c68611be53698
Lord-Valen <lord_valen@protonmail.com>
2025-07-27 21:35:05 -0400

    remove checkUser

--- dendrite/modules/lib/importForUser.nix
+++ dendrite/modules/lib/importForUser.nix
@@ -1,18 +1,4 @@
 { lib, ... }:
-let
-  /*
-    TODO: Assert that user is defined.
-    Currently we check whether the user exists
-    - i.e. their module is imported -
-    but we want to assert that there is at least a module for that user.
-    If there isn't, then there is a typo or it is dead code.
-
-    This means I will have to rewrite my users such that they have their
-    own namespace `flake.modules.users` and are an attrset of the different
-    module namespaces e.g. `user.nixos` and `user.home-manager`.
-  */
-  checkUser = user: nixosConfig: nixosConfig.home-manager.users ? user;
-in
 {
   /**
     Create a nixos module which imports a home-manager module, `module`, for `user`.
@@ -29,8 +15,7 @@ in
       config,
       ...
     }:
-    # Assert first to help keep typos from creeping in.
-    lib.mkIf (checkUser user config) {
+    lib.mkIf (config ? home-manager) {
       home-manager.users.${user}.imports = [
         module
       ];
@@ -51,7 +36,7 @@ in
       config,
       ...
     }:
-    lib.mkIf (checkUser user config) {
-      home-manager.users.${user}.imports = modules;
+    {
+      home-manager.users.${user}.imports = lib.optionals (config ? home-manager) modules;
     };
 }
