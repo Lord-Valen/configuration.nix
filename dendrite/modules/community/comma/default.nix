a778d3811f71dacbeac4c0f51d5513f4ff4225a4
Lord-Valen <lord_valen@protonmail.com>
2025-07-28 01:12:25 -0400

    use non-deprecated module attr

--- dendrite/modules/community/comma/default.nix
+++ dendrite/modules/community/comma/default.nix
@@ -1,7 +1,7 @@
 { inputs, ... }:
 {
   flake.modules.homeManager.comma = {
-    imports = [ inputs.nix-index-database.hmModules.nix-index ];
+    imports = [ inputs.nix-index-database.homeModules.nix-index ];
     programs.nix-index-database = {
       comma.enable = true;
     };
