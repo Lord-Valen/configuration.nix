0cd6918f7528404e9f4d94bdfe96644a27d3ff83
Lord-Valen <lord_valen@protonmail.com>
2025-07-28 00:35:04 -0400

    add autolycus

---
+++ dendrite/modules/hosts/autolycus/disko/nvme0n1/default.nix
@@ -0,0 +1,13 @@
+{
+  flake.modules.hosts.autolycus.disko.devices.disk.nvme0n1 = {
+    device = "/dev/nvme0n1";
+    type = "disk";
+    content = {
+      type = "gpt";
+      partitions = {
+        EFI = import ./_EFI.nix;
+        MAIN = import ./_MAIN.nix;
+      };
+    };
+  };
+}
