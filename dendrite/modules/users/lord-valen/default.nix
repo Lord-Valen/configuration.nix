63294dbd6bc17273f111d065df2ced56cfd961fb
Lord-Valen <lord_valen@protonmail.com>
2025-07-27 21:38:03 -0400

    refactor user

--- dendrite/modules/users/lord-valen/default.nix
+++ dendrite/modules/users/lord-valen/default.nix
@@ -11,33 +11,34 @@ in
 {
   flake.modules = {
     nixos.${username} =
-      { pkgs, config, ... }:
+      { pkgs, ... }:
       {
-        users.users.${username} = lib.mkMerge [
-          {
-            initialHashedPassword = "$6$nVSlPb7ImRaYAkid$xMyn6KfAw1r9KqOZrZB8ldfk5zNZSU7U/QKST.M218dkzxGbXLf/7RzQflpH.csU6vubGG21QqRRqv8yKrsdb0";
-            isNormalUser = true;
-            createHome = true;
-            # extraGroups = [
-            #   "adbusers"
-            #   "libvirtd"
-            #   "networkmanager"
-            #   "wheel"
-            #   "audio"
-            #   "wireshark"
-            #   config.services.kubo.group
-            # ];
-            openssh.authorizedKeys.keys = [
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJlTDo77mX1eDjo5o44C9pvIt+8nOptLVJQoGr1/Ilgl" # cardno:25_313_700
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICpUIUwaDNzvZJpkhhsK/yN1DaMCqhpmDFILhXG1kfOr" # cardno:20_624_908
-            ];
-            shell = pkgs.nushell;
-          }
-          (lib.mkIf (config ? home-manager) self.lib.importManyForUser username [
-            modules.home-manager.${username}
-            modules.home-manager.nushell
-          ])
-        ];
+        users.users.${username} = {
+          initialHashedPassword = "$6$nVSlPb7ImRaYAkid$xMyn6KfAw1r9KqOZrZB8ldfk5zNZSU7U/QKST.M218dkzxGbXLf/7RzQflpH.csU6vubGG21QqRRqv8yKrsdb0";
+          isNormalUser = true;
+          createHome = true;
+          # extraGroups = [
+          #   "adbusers"
+          #   "libvirtd"
+          #   "networkmanager"
+          #   "wheel"
+          #   "audio"
+          #   "wireshark"
+          #   config.services.kubo.group
+          # ];
+          openssh.authorizedKeys.keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJlTDo77mX1eDjo5o44C9pvIt+8nOptLVJQoGr1/Ilgl" # cardno:25_313_700
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICpUIUwaDNzvZJpkhhsK/yN1DaMCqhpmDFILhXG1kfOr" # cardno:20_624_908
+          ];
+          shell = pkgs.nushell;
+        };
+        imports =
+          lib.singleton
+          <| self.lib.importManyForUser username [
+            modules.homeManager.${username}
+            modules.homeManager.nushell
+            modules.homeManager.comma
+          ];
       };
     homeManager.${username} = {
       home = {
