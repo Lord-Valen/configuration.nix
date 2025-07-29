811828135e7d6ae0f48373a5ba9f2f708887abad
Lord-Valen <lord_valen@protonmail.com>
2025-07-29 18:43:02 -0400

    add missing tools

--- dendrite/modules/community/base.nix
+++ dendrite/modules/community/base.nix
@@ -3,10 +3,46 @@
   flake.modules.nixos.base =
     { pkgs, ... }:
     {
-      environment.systemPackages = with pkgs; [ git ];
+      environment.systemPackages = with pkgs; [
+        binutils
+        coreutils
+        dnsutils
+        inetutils
+        iputils
+        usbutils
+        utillinux
+        pciutils
+        gptfdisk
+        dosfstools
+        ripgrep
+        fd
+        file
+        whois
+        bottom
+      ];
       hardware = {
         graphics.enable32Bit = lib.mkDefault true;
         enableRedistributableFirmware = lib.mkDefault true;
+      };
+      programs = {
+        nix-ld.enable = true;
+        git.enable = true;
+        nh = {
+          enable = true;
+          clean = {
+            enable = true;
+            dates = "weekly";
+            extraArgs = "--keep 2 --keep-since 2d";
+          };
+        };
+      };
+      services = {
+        openssh = {
+          enable = true;
+          settings.PasswordAuthentication = false;
+        };
+        earlyoom.enable = true;
+        udisks2.enable = true;
       };
     };
 }
