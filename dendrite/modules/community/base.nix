c3700dbc3a54a7a8a055d7e3a551b7c7bfdc8674
Lord-Valen <lord_valen@protonmail.com>
2025-07-28 23:54:41 -0400

    add development module

--- dendrite/modules/community/base.nix
+++ dendrite/modules/community/base.nix
@@ -1,9 +1,12 @@
 { lib, ... }:
 {
-  flake.modules.nixos.base = {
-    hardware = {
-      graphics.enable32Bit = lib.mkDefault true;
-      enableRedistributableFirmware = lib.mkDefault true;
+  flake.modules.nixos.base =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = with pkgs; [ git ];
+      hardware = {
+        graphics.enable32Bit = lib.mkDefault true;
+        enableRedistributableFirmware = lib.mkDefault true;
+      };
     };
-  };
 }
---
+++ dendrite/modules/users/lord-valen/development.nix
@@ -0,0 +1,47 @@
+{ self, config, ... }:
+{
+  flake.modules.nixos.development =
+    with config.flake.modules.homeManager;
+    [
+      development
+      "lord-valen/development"
+    ]
+    |> self.lib.importManyForUser "lord-valen";
+
+  flake.modules.homeManager."lord-valen/development" =
+    { config, ... }:
+    {
+      programs.jujutsu = {
+        inherit (config.programs.git) userName userEmail;
+      };
+      programs.git = {
+        userName = "Lord-Valen";
+        userEmail = "lord_valen@protonmail.com";
+
+        extraConfig = {
+          github.user = "Lord-Valen";
+          gitlab.user = "Lord-Valen";
+          init.defaultBranch = "main";
+          merge.conflictStyle = "zdiff3";
+
+          fetch = {
+            prune = true;
+            pruneTags = true;
+          };
+
+          pull.rebase = true;
+          push = {
+            autoSetupRemote = true;
+            default = "current";
+          };
+
+          remote.pushDefault = "Lord-Valen";
+        };
+
+        sighning = {
+          key = "C5129E27E5CCA729";
+          signByDefault = true;
+        };
+      };
+    };
+}
