1f3b66d5bec8cccfe5632fb68cb5cc0ec6ffc5b5
Lord-Valen <lord_valen@protonmail.com>
2025-07-31 05:05:51 -0400

    use run command to respect dry runs

--- dendrite/modules/community/appimage.nix
+++ dendrite/modules/community/appimage.nix
@@ -14,7 +14,7 @@
         in
         {
           activation.createApplicationDirectory = hm.dag.entryAfter [ "writeBoundary" ] ''
-            [[ -L "${dir}" ]] || mkdir -p "$VERBOSE_ARG" "${dir}"
+            [[ -a "${dir}" ]] || run mkdir -p "$VERBOSE_ARG" "${dir}"
           '';
           sessionPath = [ dir ];
         };
