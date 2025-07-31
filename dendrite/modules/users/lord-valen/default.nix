db14267f34b237e35e040997edf2bec67dca0336
Lord-Valen <lord_valen@protonmail.com>
2025-07-31 04:21:35 -0400

    add groups

--- dendrite/modules/users/lord-valen/default.nix
+++ dendrite/modules/users/lord-valen/default.nix
@@ -17,15 +17,16 @@ in
           initialHashedPassword = "$6$nVSlPb7ImRaYAkid$xMyn6KfAw1r9KqOZrZB8ldfk5zNZSU7U/QKST.M218dkzxGbXLf/7RzQflpH.csU6vubGG21QqRRqv8yKrsdb0";
           isNormalUser = true;
           createHome = true;
-          # extraGroups = [
-          #   "adbusers"
-          #   "libvirtd"
-          #   "networkmanager"
-          #   "wheel"
-          #   "audio"
-          #   "wireshark"
-          #   config.services.kubo.group
-          # ];
+          extraGroups = [
+            # TODO: move these to modules
+            #   "adbusers"
+            #   "libvirtd"
+            "networkmanager"
+            "wheel"
+            "audio"
+            #   "wireshark"
+            #   config.services.kubo.group
+          ];
           openssh.authorizedKeys.keys = [
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJlTDo77mX1eDjo5o44C9pvIt+8nOptLVJQoGr1/Ilgl" # cardno:25_313_700
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICpUIUwaDNzvZJpkhhsK/yN1DaMCqhpmDFILhXG1kfOr" # cardno:20_624_908
