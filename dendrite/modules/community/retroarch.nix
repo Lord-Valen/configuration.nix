dcdd5b9da47a4ce0c8d0ab1a77fb45f40a78f27e
Lord-Valen <lord_valen@protonmail.com>
2025-10-25 06:39:06 -0400

    add retroarch

---
+++ dendrite/modules/community/retroarch.nix
@@ -0,0 +1,22 @@
+{
+  flake.modules.nixos.retroarch =
+    { pkgs, ... }:
+    {
+      services.xserver.desktopManager.retroarch = {
+        enable = true;
+        package = pkgs.retroarch.withCores (
+          cores: with cores; [
+            scummvm
+            beetle-psx
+            ppsspp
+            pcsx2
+            mgba
+            mesen
+            bsnes
+            mupen64plus
+            blastem
+          ]
+        );
+      };
+    };
+}
