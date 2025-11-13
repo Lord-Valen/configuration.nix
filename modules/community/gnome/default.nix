4303c1338c55918011be18c32a5220aabf719334
Lord-Valen <lord_valen@protonmail.com>
2025-11-12 20:11:28 -0500

    use flake-aspects for gnome

--- modules/community/gnome/default.nix
+++ modules/community/gnome/default.nix
@@ -1,33 +1,33 @@
 {
-  flake.modules.nixos.gnome = {
-    xdg.portal.enable = true;
+  flake.aspects.gnome = {
+    nixos = {
+      xdg.portal.enable = true;
 
-    services.libinput = {
-      enable = true;
-      touchpad.naturalScrolling = true;
+      services.libinput = {
+        enable = true;
+        touchpad.naturalScrolling = true;
+      };
+
+      services.desktopManager.gnome.enable = true;
     };
-
-    services.desktopManager.gnome.enable = true;
+    homeManager =
+      { pkgs, ... }:
+      {
+        programs.gnome-shell = {
+          enable = true;
+          extensions =
+            with pkgs.gnomeExtensions;
+            map (package: { inherit package; }) [
+              paperwm
+              switcher
+              system-monitor
+              status-icons
+              lilypad
+            ];
+        };
+      };
   };
-
   flake.modules.nixos.stylix = {
     stylix.targets.qt.enable = false;
   };
-
-  flake.modules.homeManager.gnome =
-    { pkgs, ... }:
-    {
-      programs.gnome-shell = {
-        enable = true;
-        extensions =
-          with pkgs.gnomeExtensions;
-          map (package: { inherit package; }) [
-            paperwm
-            switcher
-            system-monitor
-            status-icons
-            lilypad
-          ];
-      };
-    };
 }
