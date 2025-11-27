8c8e0dc9a66a036abcd4ebba6e71b79367b57793
Lord-Valen <lord_valen@protonmail.com>
2025-11-27 11:50:17 -0500

    add navidrome

---
+++ modules/community/servarr/navidrome.nix
@@ -0,0 +1,24 @@
+{
+  flake.modules.nixos.servarr =
+    { lib, config, ... }:
+    {
+      services = {
+        cloudflare-dyndns.domains = [ "navidrome.laughing-man.xyz" ];
+        nginx.virtualHosts."navidrome.laughing-man.xyz" = {
+          forceSSL = lib.mkDefault config.security.acme.acceptTerms;
+          enableACME = lib.mkDefault config.security.acme.acceptTerms;
+          locations = {
+            "/".proxyPass = "http://localhost:4533";
+            "/share".proxyPass = "http://localhost:8686/share/";
+          };
+        };
+
+        navidrome = {
+          enable = true;
+          settings = {
+            MusicFolder = "/data/media/music";
+          };
+        };
+      };
+    };
+}
