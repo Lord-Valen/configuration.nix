faf20020ed88a530bea2ecbe72a64583039202bd
Lord-Valen <lord_valen@protonmail.com>
2025-11-27 14:52:32 -0500

    stop using qgroups

--- modules/hosts/theseus/disk/hdds/snapper.nix
+++ modules/hosts/theseus/disk/hdds/snapper.nix
@@ -2,12 +2,12 @@
   flake.modules.hosts.theseus.services.snapper.configs = {
     data = {
       SUBVOLUME = "/data";
-      QGROUP = "1/5400";
+      # QGROUP = "1/5400";
       TIMELINE_CREATE = true;
       TIMELINE_CLEANUP = true;
       EMPTY_PRE_POST_CLEANUP = true;
-      TIMELINE_LIMIT_HOURLY = "1-2";
-      TIMELINE_LIMIT_DAILY = "0-5";
+      TIMELINE_LIMIT_HOURLY = "1";
+      TIMELINE_LIMIT_DAILY = "1";
       TIMELINE_LIMIT_WEEKLY = "0";
       TIMELINE_LIMIT_MONTHLY = "0";
       TIMELINE_LIMIT_YEARLY = "0";
