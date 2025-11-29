792c32bac361ae0c4be7873725181401561d9807
Lord-Valen <lord_valen@protonmail.com>
2025-11-28 20:48:52 -0500

    import syncthing for sioux

--- modules/hosts/weeping-willow/syncthing.nix
+++ modules/hosts/weeping-willow/syncthing.nix
@@ -6,30 +6,10 @@ in
   flake.modules.hosts.weeping-willow = {
     imports = with modules.nixos; [
       syncthing
-      (self.lib.importForUser "lord-valen" modules.homeManager.syncthing)
+      (self.lib.importForUser "sioux" modules.homeManager.syncthing)
     ];
 
     services.syncthing.settings.folders = {
-      "Pythia Bup" = {
-        id = "jtafu-4mn0y";
-        path = "~/pythia-bup";
-        type = "receiveonly";
-        devices = [
-          "Autolycus"
-          "Theseus"
-          "Pythia"
-        ];
-      };
-      "Pythia Photos" = {
-        id = "pixel_7_n835-photos";
-        path = "~/pythia-photos";
-        type = "receiveonly";
-        devices = [
-          "Autolycus"
-          "Theseus"
-          "Pythia"
-        ];
-      };
     };
   };
 }
