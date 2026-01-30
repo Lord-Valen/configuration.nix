0843966ded74a4909315d7e1b56b73f556afe3ed
Lord-Valen <lord_valen@protonmail.com>
2026-01-29 23:39:17 -0500

    update elpaca

--- modules/community/emacs/_src/init.el
+++ modules/community/emacs/_src/init.el
@@ -1,6 +1,6 @@
 ;;; init.el -*- lexical-binding:t ; -*-
 ;; init elpaca
-(defvar elpaca-installer-version 0.10)
+(defvar elpaca-installer-version 0.11)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
@@ -35,7 +35,7 @@
   (unless (require 'elpaca-autoloads nil t)
     (require 'elpaca)
     (elpaca-generate-autoloads "elpaca" repo)
-    (load "./elpaca-autoloads")))
+    (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
 (add-hook 'after-init-hook #'elpaca-process-queues)
 (elpaca `(,@elpaca-order))
 
