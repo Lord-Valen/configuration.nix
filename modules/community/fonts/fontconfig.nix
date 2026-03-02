5feb87bfaede3685745d9c3752897306e29ee1b0
Lord-Valen <lord_valen@protonmail.com>
2026-03-02 16:35:34 -0500

    add cjk fonts

--- modules/community/fonts/fontconfig.nix
+++ modules/community/fonts/fontconfig.nix
@@ -1,5 +1,36 @@
 {
-  flake.modules.homeManager.fonts = {
-    fonts.fontconfig.enable = true;
-  };
+  flake.modules.homeManager.fonts =
+    { pkgs, ... }:
+    {
+      fonts.fontconfig.enable = true;
+      fonts.fontconfig.defaultFonts =
+
+        {
+          sansSerif = [
+            "Iosevka Aile"
+            "Sarasa Gothic JP"
+          ];
+          serif = [
+            "Iosevka Etoile"
+            "Sarasa Gothic JP"
+          ];
+          monospace = [
+            "Iosevka Term SS05"
+            "Sarasa Term JP"
+          ];
+        };
+      home.packages =
+        let
+          iosevka-aile = pkgs.iosevka-bin.override { variant = "Aile"; };
+          iosevka-etoile = pkgs.iosevka-bin.override { variant = "Etoile"; };
+          iosevka-ss05 = pkgs.iosevka-bin.override { variant = "SS05"; };
+        in
+        with pkgs;
+        [
+          iosevka-aile
+          iosevka-etoile
+          iosevka-ss05
+          pkgs.sarasa-gothic
+        ];
+    };
 }
