4195eca5ccd955e6363cabb86fec4041dc0318d0
Lord-Valen <lord_valen@protonmail.com>
2026-07-06 17:02:23 -0400

    aria2: add downloadAccessUsers option; grant lord-valen read access via tmpfiles ACL

--- modules/community/aria2.nix
+++ modules/community/aria2.nix
@@ -2,9 +2,15 @@
   den.aspects.aria2.nixos =
     { config, lib, ... }:
     {
-      options.den.services.aria2.rpcSecretFile = lib.mkOption {
-        type = lib.types.nullOr lib.types.path;
-        default = null;
+      options.den.services.aria2 = {
+        rpcSecretFile = lib.mkOption {
+          type = lib.types.nullOr lib.types.path;
+          default = null;
+        };
+        downloadAccessUsers = lib.mkOption {
+          type = lib.types.listOf lib.types.str;
+          default = [ ];
+        };
       };
 
       config = lib.mkMerge [
@@ -18,9 +24,16 @@
               split = 16;
             };
           };
-          systemd.tmpfiles.rules = [
-            "e ${config.services.aria2.settings.dir} - - - 2w"
-          ];
+          systemd.tmpfiles.rules =
+            let
+              dir = config.services.aria2.settings.dir;
+              stateDir = "/var/lib/aria2";
+              aclRules = map (user: [
+                "a+ ${stateDir} - - - - u:${user}:x"
+                "a+ ${dir} - - - - u:${user}:rx"
+              ]) config.den.services.aria2.downloadAccessUsers;
+            in
+            [ "e ${dir} - - - 2w" ] ++ lib.flatten aclRules;
         })
         (lib.mkIf (config.den.services.aria2.rpcSecretFile == null) {
           warnings = [ "den.aspects.aria2: no rpcSecretFile set, service disabled" ];
--- modules/hosts/autolycus/services/aria2.nix
+++ modules/hosts/autolycus/services/aria2.nix
@@ -9,6 +9,7 @@
           owner = "aria2";
         };
         den.services.aria2.rpcSecretFile = config.sops.secrets.aria2_rpc_secret.path;
+        den.services.aria2.downloadAccessUsers = [ "lord-valen" ];
       };
   };
 }
